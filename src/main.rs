9e35fe4af32a8453f1746c233828bfc628d3b6f2
Alex Krantz <alex@krantz.dev>
2021-05-29 13:46:07 -0700

    Add main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
+use anyhow::{Context, Result};
+use structopt::StructOpt;
+use tokio::fs;
+
 mod args;
 mod config;
 mod http;
 
-fn main() {
-    println!("Hello, world!");
+use args::Args;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Parse the cli
+    let cli = Args::from_args();
+
+    // Get the configuration
+    let configuration = config::parse(cli.config)
+        .await
+        .context("Failed to load configuration")?;
+    let address = cli.address.unwrap_or(configuration.server.address);
+
+    // Ensure the clone directory exists
+    if !configuration.github.clone_to.exists() {
+        fs::create_dir_all(&configuration.github.clone_to)
+            .await
+            .context("Failed to create configuration clone directory")?;
+    }
+
+    // Setup the routes and launch the server
+    let routes = http::routes();
+    warp::serve(routes).run(address).await;
+
+    Ok(())
 }
