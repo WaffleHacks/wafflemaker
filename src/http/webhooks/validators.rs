1590bf51f674dce59990cdd12aedb8a00985abd8
Alex Krantz <alex@krantz.dev>
2021-05-29 16:04:30 -0700

    Use rejection in validators

--- src/http/webhooks/validators.rs
+++ src/http/webhooks/validators.rs
@@ -1,33 +1,33 @@
+use crate::http::errors::AuthorizationError;
 use ring::hmac;
-
-// TODO: use real error type
+use warp::{reject, Rejection};
 
 /// Ensure that the authorization header is correct
-pub fn docker(raw_header: String, token: String) -> Result<(), String> {
+pub fn docker(raw_header: String, token: String) -> Result<(), Rejection> {
     // Extract the base 64 portion
     let b64 = raw_header
         .strip_prefix("Basic ")
-        .ok_or_else(|| String::from("unauthorized"))?;
+        .ok_or_else(|| reject::custom(AuthorizationError))?;
 
     // Parse the base64 encoded header
-    let decoded = base64::decode(b64).map_err(|e| e.to_string())?;
-    let header = String::from_utf8(decoded).map_err(|e| e.to_string())?;
+    let decoded = base64::decode(b64).map_err(|_| reject::custom(AuthorizationError))?;
+    let header = String::from_utf8(decoded).map_err(|_| reject::custom(AuthorizationError))?;
 
     // Check the tokens match
     if header != token {
-        Err(String::from("unauthorized"))
+        Err(reject::custom(AuthorizationError))
     } else {
         Ok(())
     }
 }
 
 /// Ensure that the provided signature from GitHub is valid
-pub fn github(raw_body: &[u8], raw_signature: String, secret: &[u8]) -> Result<(), String> {
+pub fn github(raw_body: &[u8], raw_signature: String, secret: &[u8]) -> Result<(), Rejection> {
     // Remove the `sha256=` prefix from the hash
     let signature_hex = raw_signature
         .strip_prefix("sha256=")
-        .ok_or_else(|| String::from("unauthorized"))?;
-    let signature = hex::decode(signature_hex).map_err(|e| e.to_string())?;
+        .ok_or_else(|| reject::custom(AuthorizationError))?;
+    let signature = hex::decode(signature_hex).map_err(|_| reject::custom(AuthorizationError))?;
 
     let key = hmac::Key::new(hmac::HMAC_SHA256, secret);
 
@@ -40,7 +40,7 @@ pub fn github(raw_body: &[u8], raw_signature: String, secret: &[u8]) -> Result<(
     );
 
     // Verify the signature
-    hmac::verify(&key, raw_body, &signature).map_err(|e| e.to_string())
+    hmac::verify(&key, raw_body, &signature).map_err(|_| reject::custom(AuthorizationError))
 }
 
 #[cfg(test)]
