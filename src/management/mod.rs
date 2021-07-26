c5043bc47f524836a508445cb7c4c1247ede6b5d
Alex Krantz <alex@krantz.dev>
2021-07-25 22:56:33 -0700

    Add authentication filter

--- src/management/mod.rs
+++ src/management/mod.rs
@@ -2,7 +2,7 @@ use super::config;
 use tokio::sync::broadcast::Sender;
 use tokio::task;
 use tracing::{info, instrument};
-use warp::{Error, Filter};
+use warp::{Error, Filter, Rejection};
 
 /// Start the management interface
 #[instrument(skip(stop_tx))]
@@ -15,7 +15,7 @@ pub fn start(stop_tx: Sender<()>) -> Result<(), Error> {
     }
 
     // Build the routes
-    let routes = warp::path("test").map(|| "test");
+    let routes = authentication(&config.token).and(warp::path("test").map(|| "test"));
     let (address, server) =
         warp::serve(routes).try_bind_with_graceful_shutdown(config.address, async move {
             stop_tx.subscribe().recv().await.ok();
@@ -26,4 +26,23 @@ pub fn start(stop_tx: Sender<()>) -> Result<(), Error> {
     info!("management interface listening on {}", address);
 
     Ok(())
+}
+
+/// Check the authentication header
+fn authentication(
+    expected_token: &'static str,
+) -> impl Filter<Extract = (), Error = Rejection> + Copy {
+    warp::header::<String>("Authorization")
+        .and_then(move |header: String| async move {
+            let token = header
+                .strip_prefix("Bearer")
+                .ok_or(warp::reject::not_found())?; // TODO: better errors
+
+            if token != expected_token {
+                Err(warp::reject::not_found()) // TODO: better errors
+            } else {
+                Ok(())
+            }
+        })
+        .untuple_one()
 }
