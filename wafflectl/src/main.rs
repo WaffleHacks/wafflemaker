48919f79bfca366fd9cf639719175f41d322c887
Alex Krantz <alex@krantz.dev>
2021-07-27 23:18:10 -0700

    Add pretty error handling

--- wafflectl/src/main.rs
+++ wafflectl/src/main.rs
@@ -1,10 +1,20 @@
+use eyre::{Result, WrapErr};
 use structopt::StructOpt;
 
 mod args;
 
 use args::Args;
 
-fn main() {
+fn main() -> Result<()> {
+    // Setup traceback
+    if std::env::var("RUST_SPANTRACE").is_err() {
+        std::env::set_var("RUST_SPANTRACE", "0");
+    }
+    color_eyre::install()?;
+
+    // Parse the CLI
     let cli = Args::from_args();
     println!("{:?}", cli);
+
+    Ok(())
 }
