76afa7f451285fadde6ef213e64049549d97e19a
Alex Krantz <alex@krantz.dev>
2022-05-02 11:26:35 -0700

    Fix record serialization (again)

--- src/dns/records.rs
+++ src/dns/records.rs
@@ -4,14 +4,14 @@ const TTL: usize = 60;
 
 #[derive(Serialize)]
 pub(crate) struct Records<'r> {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    a: Option<ARecord<'r>>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    a: Vec<ARecord<'r>>,
 }
 
 impl<'r> Records<'r> {
     pub fn a_record(ip: &'r str) -> Self {
         Self {
-            a: Some(ARecord { ip, ttl: TTL }),
+            a: vec![ARecord { ip, ttl: TTL }],
         }
     }
 }
